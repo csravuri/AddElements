6fa9cec10a221dd1877a1c5487701d13a8b15b7b
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2019-05-05 03:00:47 +0530

    Simple sum application
    With Java, GUI

--- AddElements/src/com/example/addelements/SumElements.java
+++ AddElements/src/com/example/addelements/SumElements.java
@@ -1,9 +1,70 @@
 package com.example.addelements;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import com.sun.xml.internal.ws.commons.xmlutil.Converter;
 
 public class SumElements {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		JFrame frame = new JFrame("Sum of Elements");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 100);
+		
+		JButton button = new JButton("Sum");
+		JTextField textBox1 = new JTextField(10);
+		JTextField textBox2 = new JTextField(10);
+		JLabel label = new JLabel();
+		
+		JPanel panel = new JPanel();
+		panel.add(textBox1);
+		panel.add(textBox2);
+		panel.add(button);		
+		panel.add(label);
+		
+		frame.getContentPane().add(panel);
+		
+		frame.setVisible(true);
+		
+		
+		button.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				// TODO Auto-generated method stub
+				
+				double text1=0, text2=0;
+				try
+				{
+					text1 = Double.parseDouble(textBox1.getText());
+				}
+				catch(Exception err)
+				{
+					label.setText("Only Numbers allowed");
+				}
+				
+				try
+				{
+					text2 = Double.parseDouble(textBox2.getText());
+				}
+				catch(Exception err)
+				{
+					label.setText("Only Numbers allowed");
+				}
+				
+				if(text1 > 0 || text2 > 0)
+				{
+					label.setText(String.valueOf(text1 + text2));
+				}
+
+			}
+		});
+		
 
 	}
 
